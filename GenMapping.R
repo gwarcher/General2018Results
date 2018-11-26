3014ace3ba69f11d02bebd0f0e178987a40d3c95
garcher <garcher@azsos.gov>
2018-11-26 16:31:36 -0700

    initial Commit

---
+++ GenMapping.R
@@ -0,0 +1,153 @@
+library(tidyverse)
+library(leaflet)
+library(scales)
+library(htmltools)
+library(RColorBrewer)
+library(rgdal)
+library(rmapshaper)
+
+
+shp <- readOGR("shape/az_vtd_2018_new_pima.shp", layer="az_vtd_2018_new_pima")
+
+#shp <- ms_simplify(shp)
+#names(shp) = NULL
+#shp <- ms_simplify(shp)
+
+
+prop127 <- AZ_Precinct_Results %>%
+  filter(contestLongName == "Proposition 127") %>%
+  spread(choiceName, precinctVotes)
+
+write.csv(prop127, "prop127.csv", row.names = FALSE)
+
+
+
+############## SOS   #################
+
+
+sos <- AZ_Precinct_Results %>%
+  filter(contestLongName == "Secretary of State") %>%
+  select(contestLongName, choiceName, Pctcd, precinctVotes)%>%
+  spread(choiceName, precinctVotes)
+
+write.csv(sos, "Sos.csv", row.names = FALSE)
+
+
+#############################
+
+
+
+####SENATE  ############
+
+
+
+
+Senate <- AZ_Precinct_Results %>%
+  filter(contestLongName == "U.S. Senator") %>%
+  select(contestLongName, choiceName, Pctcd, precinctVotes)%>%
+  spread(choiceName, precinctVotes) %>%
+  mutate(total = rowSums(.[3:5])) %>%
+  mutate(color = case_when(
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .6 ~ "#660000",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .5 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .6 ~ "#881111",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .4 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .5 ~ "#aa2222",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .3 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .4 ~ "#cc3333",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .2 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .3 ~ "#eb4747",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .1 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .2 ~ "#f58181",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= .05 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .1 ~ "#ffaaae",
+    (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) >= 0 & (`McSally, Martha` / total) - (`Sinema, Kyrsten` /total) < .05 ~ "#ffd5d9",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .6 ~ "#000066",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .5 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .6 ~ "#111188",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .4 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .5 ~ "#2222aa",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .3 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .4 ~ "#3333cc",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .2 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .3 ~ "#4747eb",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .1 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .2 ~ "#8181f5",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= .05 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .1 ~ "#aaaeff",
+     (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  >= 0 & (`Sinema, Kyrsten` /total) - (`McSally, Martha` / total)  < .05 ~ "#d5d9ff"
+  ))
+
+write.csv(Senate, "senate.csv", row.names = FALSE)
+
+
+
+
+
+pct <- shp
+pct@data <- left_join(pct@data, Senate, by=c("pctnum" = "Pctcd"))
+
+
+popupdata=pct
+
+popupdata$label <-   popupdata$label <- paste0('<h4>', popupdata$precinctna, '</h4>',
+                                               '<h5>', popupdata$pctnum, '</h5>',
+                                               "County: ", popupdata$COUNTY, '</br>',
+                                               "Congressional: ", popupdata$DistCD, '</br>',
+                                               "Legislative: ", popupdata$DistLD, '</br>',
+                                               "Total Votes: ", popupdata$total, '</br>',
+                                               "McSalley: ", popupdata$`McSally, Martha`, " (", percent(popupdata$`McSally, Martha` / popupdata$total), ")", '</br>',
+                                               "Sinema: ", popupdata$`Sinema, Kyrsten`, " (" ,percent(popupdata$`Sinema, Kyrsten` / popupdata$total), ")",'</br>', ")", '</br>') %>%
+  lapply(HTML)
+
+s <- leaflet(popupdata) %>%
+  setView(lng= -111.093735, lat = 34.048927, zoom = 7) %>%
+  addProviderTiles(provider = providers$Stamen.TonerLite) %>%
+  addPolygons(stroke = TRUE, weight = 2, color = popupdata$color, fillOpacity = 0.5, smoothFactor = 0.5, label=popupdata$label,  
+              labelOptions = labelOptions(opacity = 0.85,
+                                          style = list(
+                                            "font-size" = "12px"
+                                          ))) %>%
+  addLegend("bottomright",
+            colors = c("#660000", "#881111", "#aa2222", "#cc3333", "#eb4747", "#f58181", "#ffaaae", "#ffd5d9",
+                       "#000066", "#111188", "#2222aa", "#3333cc", "#4747eb", "#8181f5", "#aaaeff", "#d5d9ff",
+                       "DFDFDF"),
+            labels = c("McSally +60%", "+50%", "+40%", "+30%", "+20%", "+10%", "+5%", "+0%",
+                       "Sinema + 60%",  "+50%", "+40%", "+30%", "+20%", "+10%", "+5%", "+0%",
+                       "No Returns"),
+            title = "Sinema v McSally",
+            opacity = 0.8)
+
+
+
+
+#### CORP COMM ####
+
+CorpComm <- AZ_Precinct_Results %>%
+  filter(contestLongName == "Corporation Commissioner") %>%
+  select(contestLongName, choiceName, Pctcd, precinctVotes)%>%
+  spread(choiceName, precinctVotes) %>%
+  mutate(total = rowSums(.[3:6])) %>%
+  mutate(color = case_when(
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .6 ~ "#660000",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .5 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .6 ~ "#881111",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .4 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .5 ~ "#aa2222",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .3 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .4 ~ "#cc3333",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .2 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .3 ~ "#eb4747",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .1 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .2 ~ "#f58181",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= .05 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .1 ~ "#ffaaae",
+    (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) >= 0 & (`Glassman, Rodney` / total) - (`Kennedy, Sandra` / total) < .05 ~ "#ffd5d9",
+    (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .6 ~ "#000066",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .5 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .6 ~ "#111188",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .4 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .5 ~ "#2222aa",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .3 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .4 ~ "#3333cc",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .2 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .3 ~ "#4747eb",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .1 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .2 ~ "#8181f5",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= .05 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .1 ~ "#aaaeff",
+   (`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) >= 0 &(`Kennedy, Sandra` / total) - (`Glassman, Rodney` / total) < .05 ~ "#d5d9ff"
+  ))
+
+
+write.csv(CorpComm, "corpcomm.csv", row.names = FALSE)
+
+pct <- shp
+pct@data <- inner_join(pct@data, CorpComm, by=c("pctnum" = "Pctcd"))
+
+
+cc <- leaflet(pct) %>%
+  setView(lng= -111.093735, lat = 34.048927, zoom = 7) %>%
+  addProviderTiles(provider = providers$Stamen.TonerLite) %>%
+  addPolygons(stroke = TRUE, weight = 2, color = pct@data$color, fillOpacity = 0.5, smoothFactor = 0.5,  
+              labelOptions = labelOptions(opacity = 0.85,
+                                          style = list(
+                                            "font-size" = "12px"
+                                          )))
+  
